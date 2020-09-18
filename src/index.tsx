b7b468dc8b3e9035b6a1f4d38af7f263cb020f4a
Ahmad Sadiq <ahmadsadiq885@gmail.com>
2020-09-18 15:41:25 +0530

    first commit"

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import firebase from 'firebase';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyCouK_8wxedZtmvN5A1a97fM4omxRUiOnA',
+  authDomain: 'feedhub885.firebaseapp.com',
+  databaseURL: 'https://feedhub885.firebaseio.com',
+  projectId: 'feedhub885',
+  storageBucket: 'feedhub885.appspot.com',
+  messagingSenderId: '46501347455',
+  appId: '1:46501347455:web:18bd370654dd860ca6ff4a',
+  measurementId: 'G-8FX707LTXR',
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
